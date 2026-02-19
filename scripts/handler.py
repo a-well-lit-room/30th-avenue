49b62438892bd22ce9ce3d60b4bff846b02e8a97
a-well-lit-room <kaelanburkett@gmail.com>
2026-02-18 21:01:04 -0500

    new videos and starting a handler file for all video inputs

---
+++ scripts/handler.py
@@ -0,0 +1,11 @@
+# look at directory or file
+
+# find out if the file is already uploaded (using checksum)
+
+# identify the metadata for each file
+
+# verify if file was recorded around 30th Ave
+
+# if it is valid, compress the video and add metadata to json file, including filename, creation time, creation location, and source checksum
+
+# add an option to stretch video if it is too short
